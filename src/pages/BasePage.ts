34a400e64255160f2c949a6a905874087756ce25
Lukas Roels <lukas.roels@hotmail.com>
2026-04-30 16:32:13 +0200

    better

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -19,7 +19,6 @@ export class BasePage {
 
     async acceptCookies() {
         await this.page.getByRole('button', {name: 'Accept All'}).click();
-        await this.page.waitForLoadState('load');
         await this.page.getByRole('button', {name: 'Consent', exact: true}).click();
     }
 
