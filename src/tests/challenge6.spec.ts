c84066b21af31a9521ebfc3124c349969c9cd565
LukasRoelsDeloitte <luroels@deloitte.com>
2026-05-18 09:20:42 +0200

    fix

--- src/tests/challenge6.spec.ts
+++ src/tests/challenge6.spec.ts
@@ -1,36 +1,25 @@
-import { expect, test } from '@playwright/test';
-import {loginPage, shopPage, productPage, cartPage, checkoutPage, orderPage, setupHooks, userData, ProductData} from '../support/E2ELoginHooks';
+import { test } from '@playwright/test';
+​import {userData, ProductData, loginPage, shopPage, productPage, cartPage, checkoutPage, orderPage, setupHooks} from'../support/E2ELoginHooks';​
 
+​setupHooks();
 
-setupHooks();
-
-// test('data correctly loaded', async () => {
-//     expect(userData.length).toBeGreaterThan(0);
-//     expect(ProductData.length).toBeGreaterThan(0);
-// });
-
-userData.forEach(user => {
-    test(`E2E test for user: ${user.username}`, async () => {
-        await loginPage.launch();
-        await loginPage.acceptCookies();
-        await loginPage.authenticate(user.username, user.password);
-
-        for (const product of ProductData) {
-            await shopPage.navigateToShop();
-            await shopPage.goToProduct(product.name);
-
-            await productPage.setProductQuantity(product.quantity);
-            await productPage.addToCart();
-            await productPage.navigateToCart();
-
-            await cartPage.verifyProductInCart(product.name, product.quantity);
-        }
-
-        await cartPage.checkout();
-
-        await checkoutPage.fillDetails(user);
-        await checkoutPage.placeOrder();
-        await orderPage.verifyOrderSuccess(user, ProductData);
-        await loginPage.logout();
-    });
-});
+​userData.filter(user => user.valid).forEach(user => {​    
+    test(`E2E test for user: ${user.username}`, async () => {​       
+        await loginPage.launch();​       
+        await loginPage.acceptCookies();​       
+        await loginPage.authenticate(user.username, user.password);​​        
+        for (const product of ProductData) {​           
+            if (product.valid) {​             
+                await shopPage.goToProduct(product.name);​​             
+                await productPage.setProductQuantity(product.quantity);​             
+                await productPage.addToCart();​​             
+                await cartPage.verifyProductInCart(product.name, product.quantity);​           
+            }​       
+        }​​       
+        await cartPage.checkout();​​       
+        await checkoutPage.fillDetails(user);​       
+        await checkoutPage.placeOrder();​​       
+        await orderPage.verifyOrderSuccess(user, ProductData);​       
+        await loginPage.logout();​    
+    });​}
+);​
